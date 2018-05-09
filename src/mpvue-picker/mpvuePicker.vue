cd84eb7641e2a98faae8faa0c87200a2def3e372
kuangpengfei <1633397286@qq.com>
2018-05-09 22:11:44 +0800

    directory adjustment

---
+++ src/mpvue-picker/mpvuePicker.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mpvue-picker">mpvue-picker</div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/demo/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mvpue-picker">
+    <mpvue-picker></mpvue-picker>
+  </div>
+</template>
+
+<script>
+import mpvuePicker from '@/mpvue-picker/mpvuePicker.vue';
+export default {
+  components: {
+    mpvuePicker
+  }
+};
+</script>
+
+<style>
+
+</style>
