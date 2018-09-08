2cfe69d2fc50d48bab13370160c5f7a311b74942
KuangPF <1633397286@qq.com>
2018-09-08 13:52:28 +0800

    fix:三级联动 bug

--- src/mpvue-picker/mpvuePicker.vue
+++ src/mpvue-picker/mpvuePicker.vue
@@ -143,10 +143,7 @@ export default {
         this.pickerValueMinute = minuteArray;
       } else if (this.mode === 'multiSelector') {
         this.pickerValueMulArray = valueArray;
-      } else if (
-        this.mode === 'multiLinkageSelector' &&
-        this.deepLength === 2
-      ) {
+      } else if (this.mode === 'multiLinkageSelector' && this.deepLength === 2) {
         // 两级联动
         let pickerValueMulTwoOne = [];
         let pickerValueMulTwoTwo = [];
@@ -203,13 +200,7 @@ export default {
           }
           // 第三列
           let numSecond = this.pickerValueDefault[1];
-          for (
-            let i = 0,
-              length =
-                pickerValueArray[num].children[numSecond].children.length;
-            i < length;
-            i++
-          ) {
+          for (let i = 0, length = pickerValueArray[num].children[numSecond].children.length; i < length; i++) {
             pickerValueMulThreeThree.push(
               pickerValueArray[num].children[numSecond].children[i]
             );
@@ -239,8 +230,9 @@ export default {
       this.showPicker = false;
       this._initPickerVale();
       let pickObj = {
-        value: this.pickerValue,
-        label: this._getPickerLabel(this.pickerValue, this.mode)
+        inex: this.pickerValue,
+        value: this._getPickerLabelAndValue(this.pickerValue, this.mode).value,
+        label: this._getPickerLabelAndValue(this.pickerValue, this.mode).label
       };
       this.$emit('onCancel', pickObj);
     },
@@ -248,8 +240,9 @@ export default {
       this.showPicker = false;
       this._initPickerVale();
       let pickObj = {
-        value: this.pickerValue,
-        label: this._getPickerLabel(this.pickerValue, this.mode)
+        inex: this.pickerValue,
+        value: this._getPickerLabelAndValue(this.pickerValue, this.mode).value,
+        label: this._getPickerLabelAndValue(this.pickerValue, this.mode).label
       };
       this.$emit('onConfirm', pickObj);
     },
@@ -259,8 +252,9 @@ export default {
     pickerChange(e) {
       this.pickerValue = e.mp.detail.value;
       let pickObj = {
-        value: this.pickerValue,
-        label: this._getPickerLabel(this.pickerValue, this.mode)
+        inex: this.pickerValue,
+        value: this._getPickerLabelAndValue(this.pickerValue, this.mode).value,
+        label: this._getPickerLabelAndValue(this.pickerValue, this.mode).label
       };
       this.$emit('onChange', pickObj);
     },
@@ -272,15 +266,8 @@ export default {
         if (changeValue[0] !== this.pickerValue[0]) {
           let pickerValueMulTwoTwo = [];
           // 第一列滚动第二列数据更新
-          for (
-            let i = 0,
-              length = pickerValueArray[changeValue[0]].children.length;
-            i < length;
-            i++
-          ) {
-            pickerValueMulTwoTwo.push(
-              pickerValueArray[changeValue[0]].children[i]
-            );
+          for (let i = 0, length = pickerValueArray[changeValue[0]].children.length; i < length; i++) {
+            pickerValueMulTwoTwo.push(pickerValueArray[changeValue[0]].children[i]);
           }
           this.pickerValueMulTwoTwo = pickerValueMulTwoTwo;
           // 第二列初始化为 0
@@ -296,97 +283,77 @@ export default {
         // 如果是第一列滚动
         if (changeValue[0] !== this.pickerValue[0]) {
           this.pickerValueMulThreeTwo = [];
-          for (
-            let i = 0,
-              length = pickerValueArray[changeValue[0]].children.length;
-            i < length;
-            i++
-          ) {
-            pickerValueMulThreeTwo.push(
-              pickerValueArray[changeValue[0]].children[i]
-            );
+          for (let i = 0, length = pickerValueArray[changeValue[0]].children.length; i < length; i++) {
+            pickerValueMulThreeTwo.push(pickerValueArray[changeValue[0]].children[i]);
           }
           // 重新渲染第三列
-          for (
-            let i = 0,
-              length =
-                pickerValueArray[changeValue[0]].children[changeValue[1]]
-                  .children.length;
-            i < length;
-            i++
-          ) {
-            pickerValueMulThreeThree.push(
-              pickerValueArray[changeValue[0]].children[changeValue[1]]
-                .children[i]
-            );
+          for (let i = 0, length = pickerValueArray[changeValue[0]].children[0].children.length; i < length; i++) {
+            pickerValueMulThreeThree.push(pickerValueArray[changeValue[0]].children[0].children[i]);
           }
           changeValue[1] = 0;
           changeValue[2] = 0;
-        } else {
+          this.pickerValueMulThreeTwo = pickerValueMulThreeTwo;
+          this.pickerValueMulThreeThree = pickerValueMulThreeThree;
+        } else if (changeValue[1] !== this.pickerValue[1]) {
           // 第二列滚动
           // 重新渲染第三列
           this.pickerValueMulThreeThree = [];
           pickerValueMulThreeTwo = this.pickerValueMulThreeTwo;
-          for (
-            let i = 0,
-              length =
-                pickerValueArray[changeValue[0]].children[changeValue[1]]
-                  .children.length;
-            i < length;
-            i++
-          ) {
-            pickerValueMulThreeThree.push(
-              pickerValueArray[changeValue[0]].children[changeValue[1]]
-                .children[i]
-            );
+          for (let i = 0, length = pickerValueArray[changeValue[0]].children[changeValue[1]].children.length; i < length; i++) {
+            pickerValueMulThreeThree.push(pickerValueArray[changeValue[0]].children[changeValue[1]].children[i]);
           }
-          if (changeValue[1] !== this.pickerValue[1]) {
-            changeValue[2] = 0;
-          }
+          changeValue[2] = 0;
+          this.pickerValueMulThreeThree = pickerValueMulThreeThree;
         }
-        this.pickerValueMulThreeTwo = pickerValueMulThreeTwo;
-        this.pickerValueMulThreeThree = pickerValueMulThreeThree;
         this.pickerValue = changeValue;
       }
       let pickObj = {
-        value: this.pickerValue,
-        label: this._getPickerLabel(this.pickerValue, this.mode)
+        inex: this.pickerValue,
+        value: this._getPickerLabelAndValue(this.pickerValue, this.mode).value,
+        label: this._getPickerLabelAndValue(this.pickerValue, this.mode).label
       };
       this.$emit('onChange', pickObj);
     },
     // 获取 pxikerLabel
-    _getPickerLabel(value, mode) {
+    _getPickerLabelAndValue(value, mode) {
       let pickerLable;
+      let pickerGetValue = [];
       // selector
       if (mode === 'selector') {
         pickerLable = this.pickerValueSingleArray[value].label;
+        pickerGetValue.push(this.pickerValueSingleArray[value].value);
       } else if (mode === 'timeSelector') {
-        pickerLable = `${this.pickerValueHour[value[0]].label}-${
-          this.pickerValueMinute[value[1]].label
-        }`;
+        pickerLable = `${this.pickerValueHour[value[0]].label}-${this.pickerValueMinute[value[1]].label}`;
+        pickerGetValue.push(this.pickerValueHour[value[0]].value);
+        pickerGetValue.push(this.pickerValueHour[value[1]].value);
       } else if (mode === 'multiSelector') {
         for (let i = 0; i < value.length; i++) {
           if (i > 0) {
-            pickerLable +=
-              this.pickerValueMulArray[i][value[i]].label +
-              (i === value.length - 1 ? '' : '-');
+            pickerLable += this.pickerValueMulArray[i][value[i]].label + (i === value.length - 1 ? '' : '-');
           } else {
             pickerLable = this.pickerValueMulArray[i][value[i]].label + '-';
           }
+          pickerGetValue.push(this.pickerValueMulArray[i][value[i]].value);
         }
       } else if (mode === 'multiLinkageSelector') {
         /* eslint-disable indent */
         pickerLable =
-          this.deepLength === 2
-            ? `${this.pickerValueMulTwoOne[value[0]].label}-${
-                this.pickerValueMulTwoTwo[value[1]].label
-              }`
-            : `${this.pickerValueMulThreeOne[value[0]].label}-${
-                this.pickerValueMulThreeTwo[value[1]].label
-              }-${this.pickerValueMulThreeThree[value[2]].label}`;
+          this.deepLength === 2 ? `${this.pickerValueMulTwoOne[value[0]].label}-${this.pickerValueMulTwoTwo[value[1]].label}`
+            : `${this.pickerValueMulThreeOne[value[0]].label}-${this.pickerValueMulThreeTwo[value[1]].label}-${this.pickerValueMulThreeThree[value[2]].label}`;
+        if (this.deepLength === 2) {
+          pickerGetValue.push(this.pickerValueMulTwoOne[value[0]].value);
+          pickerGetValue.push(this.pickerValueMulTwoTwo[value[1]].value);
+        } else {
+          pickerGetValue.push(this.pickerValueMulThreeOne[value[0]].value);
+          pickerGetValue.push(this.pickerValueMulThreeTwo[value[1]].value);
+          pickerGetValue.push(this.pickerValueMulThreeThree[value[2]].value);
+        }
         /* eslint-enable indent */
       }
-      return pickerLable;
+      return {
+        label: pickerLable,
+        value: pickerGetValue
+      };
     },
     // 初始化 pickerValue 默认值
     _initPickerVale() {
