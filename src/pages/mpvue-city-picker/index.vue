ea9049d30cc657076c380fafca03a3c0a18e3d67
KuangPF <1633397286@qq.com>
2019-05-21 22:18:35 +0800

    release: new verion 2.1.1

--- src/pages/mpvue-city-picker/index.vue
+++ src/pages/mpvue-city-picker/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="mpvue-picer">
     <button @click="showPicker">test for mpvuePicker</button>
-    <mpvue-picker ref="mpvuePicker" mode="multiLinkageSelector" :deepLength=3 @onChange="onChange" @onConfirm="onConfirm" @onCancel="onCancel" :pickerValueArray="pickerValueArray"></mpvue-picker>
+    <mpvue-picker ref="mpvuePicker" mode="multiLinkageSelector" :deepLength=3 @onChange="onChange" @onConfirm="onConfirm" @onCancel="onCancel" :pickerValueArray="pickerValueArray" :pickerValueDefault="pickerValueDefault"></mpvue-picker>
   </div>
 </template>
 
 <script>
 import mpvuePicker from 'mpvue-picker';
+// import mpvuePicker from '@/mpvue-picker/mpvuePicker.vue';
 import { cityData } from '@/pages/mpvue-city-picker/data';
 export default {
   data() {
@@ -18,6 +19,7 @@ export default {
   components: { mpvuePicker },
   methods: {
     showPicker() {
+      this.pickerValueDefault = [0, 0, 1];
       this.$refs.mpvuePicker.show();
     },
     onChange(e) {
